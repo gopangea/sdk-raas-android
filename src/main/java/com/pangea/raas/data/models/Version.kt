0b1d1d6c4d70672413ccf89c5b1ded7e412d802e
Carlos Hernandez Santes <dicarlomagnus@gmail.com>
2021-02-19 17:42:12 -0600

    adding usefull comment

--- src/main/java/com/pangea/raas/data/models/Version.kt
+++ src/main/java/com/pangea/raas/data/models/Version.kt
@@ -1,5 +1,5 @@
 package com.pangea.raas.data.models
-
+//change version number or/and build number each time you deploy a new version of the sdk
 object Version {
     const val versionNumber = "1.0.1"
     const val buildNumber = "4"
